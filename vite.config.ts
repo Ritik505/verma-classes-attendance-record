06bffeca42f6f92f6bae89e5da98af6a07584ed1
Ritik505 <rv642216@gmail.com>
2026-07-31 19:52:11 +0530

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,40 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Verma Classes Attendance',
+        short_name: 'Attendance',
+        description: 'Student Attendance Management System',
+        theme_color: '#1E3A8A',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 
   resolve: {
     alias: {
